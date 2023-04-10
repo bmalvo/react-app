7aa3756b20aa1c9a3b1ce4f98aee1cd9edf29ab3
patryk_s <boydmalvo@gmail.com>
2023-04-10 21:31:09 +0200

    ES6 Variables

--- src/index.js
+++ src/index.js
@@ -39,3 +39,22 @@ const myCar2 = new Model("Ford", "Mustang");
 // Arrow function to invitation
 
 hello = (userName) => 'Hello ' + userName + '!';
+
+// Declaring Variables
+
+// If you use var outside of a function, it belongs to the global scope.
+// If you use var inside of a function, it belongs to that function.
+// If you use var inside of a block, i.e. a for loop, the variable is still 
+//    available outside of that block.
+// var has a function scope, not a block scope.
+
+// let is the block scoped version of var, and is limited to the block
+    // (or expression) where it is defined.
+// If you use let inside of a block, i.e. a for loop, the variable is only 
+    // available inside of that loop.
+// let has a block scope.
+
+
+// const is a variable that once it has been created, its value can never change.
+// const has a block scope.
+
