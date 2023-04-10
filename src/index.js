6a3dc880041d378b3151d7a73b11d0983de10585
patryk_s <boydmalvo@gmail.com>
2023-04-10 21:14:59 +0200

    ES6 Classes

--- src/index.js
+++ src/index.js
@@ -6,3 +6,32 @@ const myFirstElement = <h1>Hello React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
+
+
+// Create class Car 
+
+class Car {
+    constructor(name){
+        this.brand = name;
+    }
+
+    present(){
+        return 'I have a ' + this.brand;
+    }
+}
+
+const myCar = new Car("Ford");
+
+// Extending class Car by class Model (inheritance)
+
+class Model extends Car{
+    constructor(name,mod){
+        super(name);
+        this.model = mod;
+    }
+    show(){
+        return this.present() + ', it is a ' + this.model
+    }
+}
+
+const myCar2 = new Model("Ford", "Mustang");
