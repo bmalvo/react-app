f5932d97661b51320538d65df17cb871cc8a7e98
patryk_s <boydmalvo@gmail.com>
2023-04-12 17:58:04 +0200

    react props

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,8 @@
 
 function Car(props){
-    return <h2>Hi! I'm a {props.color} car!</h2>
+    return <><h2>My brand is {props.brand}</h2>
+             <h2>Hi! I'm a {props.color} car!</h2>
+           </>
 }
 
 export default Car
--- src/Garage.js
+++ src/Garage.js
@@ -1,12 +1,14 @@
 import Car from './Car.js';
 
 function Garage(){
+    let color = 'blue';
+    let brand = 'mistubishi';
     return(
         <>
         <h1>Who lives in my garage?</h1>
-        <Car color="red"/>
+        <Car color={color} brand={brand}/>
         </>
-    )
+    );
 }
 
 export default Garage
