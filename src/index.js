da02697a1caab2be0e7022a26f74c34ee906cfa1
patryk_s <boydmalvo@gmail.com>
2023-04-17 20:03:49 +0200

    react styling

--- src/index.js
+++ src/index.js
@@ -6,15 +6,23 @@ import Shoot from './Shoot.js';
 import Garage2 from './Garage2.js';
 import Goal from './Goal.js';
 import MyForm from './Form.js';
+import './App.css';
 //JSX is extention of JS allows write HTML in JS
 
 let carsLoot = ["Mitsubishi", 'Peugeot', "BMW"];
 let x = 3;
 
+const myStyle = {
+    color: "white",
+    background: "Blue",
+    padding: "10px",
+    fontSize: "20px"
+};
+
 const myFirstElement = (
 <>
-<h1>Hello React!</h1>
-<p>first paragraf</p>
+<h1 style={{background: 'black',color:'white'}}>Hello React!</h1>
+<p style={myStyle}>first paragraf</p>
 <p>second paragraf <span> 'span in second paragraf'</span></p>
 <p>5 + 5 = {5+5}</p>
 <p>Hello {(x) > 1 ? "Mark" : "Twain"}</p>
